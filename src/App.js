2ed1f662d0be8ff2140213f8629b38b539c34b38
Tampoka <goal.thanks@gmail.com>
2022-06-28 15:20:24 +0900

    Add updateUser.

--- src/App.js
+++ src/App.js
@@ -5,21 +5,29 @@ import axios from 'axios';
 
 function App() {
     const [state, setState] = useState([])
+    const [name, setName] = useState("")
 
     const getUsers = () => {
         axios.get('http://localhost:4000/users')
             .then(res => setState(res.data))
     }
 
-    const addUsers = () => {
-        axios.post('http://localhost:4000/users')
+    const addUsers = (name) => {
+        axios.post('http://localhost:4000/users', {name})
+            .then(res => getUsers())
+        setName("")
+    }
+
+    const deleteUser = (id) => {
+        axios.delete(`http://localhost:4000/users/${id}`)
             .then(res => getUsers())
     }
 
-    const deleteUser = () => {
-        axios.delete('http://localhost:4000/users')
+    const updateUser = (id) => {
+        axios.put(`http://localhost:4000/users/${id}`, {name})
             .then(res => getUsers())
     }
+
     useEffect(() => {
         getUsers()
     }, [])
@@ -28,13 +36,22 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
-                <ul>
-                    Users:{state.map(u => <li key={u.id}>{u.name}
-                </li>)}
+                <p>Users:</p>
+                <ul style={{width: 300}}>
+                    {state.map(u => <li key={u.id}
+                                        style={{display: "flex", justifyContent: "space-between", paddingBottom: 10}}
+                                        onDoubleClick={() => updateUser(u.id, "Tanya")}
+                    >{u.name}
+                        {<button style={{marginLeft: 20}} onClick={() => deleteUser(u.id)}>X</button>}</li>)}
                 </ul>
                 <div>
-                    <button onClick={addUsers}>Add new user</button>
-                    <button onClick={deleteUser}>X</button>
+                    <input
+                        type="text"
+                        placeholder="Enter name"
+                        value={name}
+                        onChange={(e) => setName(e.currentTarget.value)}
+                    />
+                    <button onClick={() => addUsers(name)}>Add new user</button>
                 </div>
             </header>
         </div>
